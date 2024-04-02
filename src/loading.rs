d0f40e17b139b5c3932a35a828995edb39553415
Gavrilov Miroslav <gavrilov.miroslav@gmail.com>
2024-04-02 02:40:17 +0200

    UI grid alignment complete

--- src/loading.rs
+++ src/loading.rs
@@ -1,12 +1,11 @@
-use bevy::{app::{Plugin, Update}, asset::{Asset, AssetEvent, AssetId, AssetServer, Assets, Handle}, 
-    ecs::{event::EventReader, schedule::{common_conditions::in_state, IntoSystemConfigs, NextState, OnEnter}, 
-    system::{Commands, Res, ResMut, Resource}}, reflect::TypePath, utils::{hashbrown::{HashMap, HashSet}}};
+use bevy::{app::Plugin, core_pipeline::core_2d::Camera2dBundle, ecs::{component::Component, entity::Entity, query::With, schedule::{IntoSystemConfigs, NextState, OnEnter}, system::{Commands, Query, Res, ResMut, Resource}}, hierarchy::BuildChildren, math::{IVec2, Vec3}, render::view::{InheritedVisibility, Visibility}, sprite::{SpriteSheetBundle, TextureAtlasSprite}, transform::components::{GlobalTransform, Transform}, utils::hashbrown::HashMap, window::{PrimaryWindow, Window}};
 use bevy_asset_loader::{
     loading_state::{config::ConfigureLoadingState, LoadingState, LoadingStateAppExt},
     standard_dynamic_asset::StandardDynamicAssetCollection,
 };
 use csv::Trim;
-use std::{fmt::Debug, sync::Arc};
+use itertools::Itertools;
+use std::{collections::HashSet, fmt::Debug};
 
 use super::{GameAssets, GameStates};
 
@@ -16,11 +15,19 @@ pub struct Font {
 }
 
 #[derive(serde::Deserialize, Debug, PartialEq, Eq, Hash)]
-pub struct Glyph {
+pub struct PreGlyph {
     pub name: String,
     pub x: i32,
     pub y: i32,
     pub attributes: String,
+}
+
+#[derive(serde::Deserialize, Debug, PartialEq, Eq, Hash)]
+pub struct Glyph {
+    pub name: String,
+    pub x: i32,
+    pub y: i32,
+    pub attributes: Vec<String>,
 }
 
 #[derive(Resource, Default, Debug)]
@@ -37,17 +44,208 @@ impl Fonts {
             .from_path(format!("assets/{}", path).as_str()) else { return; };
 
         let mut font = Font::default();
-        for record in csv.deserialize::<Glyph>() {
+        for record in csv.deserialize::<PreGlyph>() {
             if let Ok(record) = record {
-                let name = record.name.clone();
-                if !font.glyphs.contains_key(&name) {
-                    font.glyphs.insert(name.clone(), HashSet::new());
+                let is_quote = record.attributes.starts_with("\"");
+                if is_quote {
+                    let attributes = record.attributes.clone();
+                    let mut attribs = attributes.chars().into_iter();
+                    attribs.next_back();
+                    attribs.next();
+                    for (dx, letter) in attribs.enumerate() {
+                        let mut name = letter.to_string();
+                        if name.as_str() == "€" {
+                            name = "|".to_string();
+                        }
+                        let glyph = Glyph {
+                            name: name.clone(),
+                            x: record.x + dx as i32,
+                            y: record.y, 
+                            attributes: vec![ name.clone() ],
+                        };
+
+                        if !font.glyphs.contains_key(&name) {
+                            font.glyphs.insert(name.clone(), HashSet::new());
+                        }
+                        font.glyphs.get_mut(&name).unwrap().insert(glyph);
+                    }
+                } else {
+                    let name = record.name.clone();
+                    if !font.glyphs.contains_key(&name) {
+                        font.glyphs.insert(name.clone(), HashSet::new());
+                    }
+                    let glyph = Glyph {
+                        name: name.clone(),
+                        x: record.x,
+                        y: record.y,
+                        attributes: record.attributes
+                            .split(';')
+                            .map(&str::trim)
+                            .map(&str::to_owned)
+                            .collect_vec(),
+                    };
+                    font.glyphs.get_mut(&name).unwrap().insert(glyph);
                 }
-                font.glyphs.get_mut(&name).unwrap().insert(record);
             }
         }
 
         self.fonts.insert(name.to_string(), font);
+    }
+}
+
+#[derive(serde::Deserialize, Debug, PartialEq, Eq, Hash)]
+#[serde(rename_all = "camelCase")]
+pub enum GridKind {
+    Glyph,
+    Entity,
+    Boolean,
+}
+
+#[derive(serde::Deserialize, Debug, PartialEq, Eq, Hash)]
+pub enum GridAlign {
+    None,
+    TopLeft,
+    BottomLeft,
+    TopRight,
+    BottomRight,
+    Top,
+    Bottom,
+    Left,
+    Right,
+    Center,
+}
+
+pub type AlignFn = Box<dyn Fn(f32, f32, f32, f32) -> f32>;
+
+impl Grid {
+    pub fn align(&self, tileset: &Tileset, window: &Window) -> Option<Vec3> {
+        let (window_width_in_px, window_height_in_px) = (window.width(), window.height());
+        let grid_width_in_chars = self.width as f32 * tileset.width as f32;
+        let grid_height_in_chars = self.height as f32 * tileset.height as f32;
+        let grid_offset_x_in_chars = (self.x * tileset.width) as f32;
+        let grid_offset_y_in_chars = (self.y * tileset.height) as f32;
+        let (one_char, one_row) = (tileset.width as f32, tileset.height as f32);
+
+        fn left(window_width_in_px: f32, one_char: f32, _grid_width_in_chars: f32, grid_offset_x_in_chars: f32) -> f32 {
+            (-window_width_in_px + one_char) * 0.5 + grid_offset_x_in_chars
+        }
+
+        fn right(window_width_in_px: f32, _one_char: f32, grid_width_in_chars: f32, grid_offset_x_in_chars: f32) -> f32 {
+            window_width_in_px * 0.5 - grid_width_in_chars - grid_offset_x_in_chars
+        }
+        
+        fn top(window_height_in_px: f32, one_row: f32, grid_height_in_chars: f32, grid_offset_y_in_chars: f32) -> f32 {
+            (window_height_in_px + one_row) * 0.5 - grid_height_in_chars - grid_offset_y_in_chars
+        }
+
+        fn bottom(window_height_in_px: f32, one_row: f32, _grid_height_in_chars: f32, grid_offset_y_in_chars: f32) -> f32 {
+            -window_height_in_px * 0.5 + grid_offset_y_in_chars + 0.5 * one_row
+        }
+
+        fn hor_center(_window_width_in_px: f32, one_char: f32, grid_width_in_chars: f32, grid_offset_x_in_chars: f32) -> f32 {
+            (-grid_width_in_chars + one_char) * 0.5 + grid_offset_x_in_chars
+        }
+
+        fn ver_center(_window_height_in_px: f32, one_row: f32, grid_height_in_chars: f32, grid_offset_y_in_chars: f32) -> f32 {
+            (-grid_height_in_chars + one_row) * 0.5 + grid_offset_y_in_chars
+        }
+
+        let hor_ver: Option<(AlignFn, AlignFn)> = match self.align {
+            GridAlign::None => None,
+            GridAlign::TopLeft => Some((Box::new(left), Box::new(top))), 
+            GridAlign::BottomLeft => Some((Box::new(left), Box::new(bottom))),
+            GridAlign::TopRight => Some((Box::new(right), Box::new(top))),
+            GridAlign::BottomRight => Some((Box::new(right), Box::new(bottom))),
+            GridAlign::Top => Some((Box::new(hor_center), Box::new(top))),
+            GridAlign::Bottom => Some((Box::new(hor_center), Box::new(bottom))),
+            GridAlign::Left => Some((Box::new(left), Box::new(ver_center))),
+            GridAlign::Right => Some((Box::new(right), Box::new(ver_center))),
+            GridAlign::Center => Some((Box::new(hor_center), Box::new(ver_center))),
+        };
+
+        hor_ver.map(|(h, v)| {
+            Vec3::new(
+                h(window_width_in_px, one_char, grid_width_in_chars, grid_offset_x_in_chars), 
+                v(window_height_in_px, one_row, grid_height_in_chars, grid_offset_y_in_chars),
+                0.0
+            )
+        })
+    }
+}
+
+#[derive(serde::Deserialize, Debug, PartialEq, Eq, Hash)]
+pub struct Grid {
+    pub name: String,
+    pub width: i32,
+    pub height: i32,
+    pub depth: i32,
+    pub x: i32,
+    pub y: i32,
+    pub kind: GridKind,
+    pub tileset: String,
+    pub align: GridAlign,
+    #[serde(skip_deserializing)]
+    pub entities: Vec<Entity>,
+    #[serde(skip_deserializing)]
+    pub entity: Option<Entity>,
+}
+
+impl Grid {
+    pub fn get(&self, x: i32, y: i32) -> Option<&Entity> {
+        self.entities.get(((self.x + y) * self.width + (self.y + x)) as usize)
+    }
+}
+
+#[derive(Resource, Default, Debug)]
+pub struct Grids {
+    pub grids: HashMap<String, Grid>,
+}
+
+impl Grids {
+    pub fn add(&mut self, path: &str) {
+        let Ok(mut csv) = csv::ReaderBuilder::new()
+            .delimiter(b'|')
+            .comment(Some(b'#'))
+            .trim(Trim::All)
+            .from_path(format!("assets/{}", path).as_str()) else { return; };
+
+        for record in csv.deserialize::<Grid>() {
+            if let Ok(record) = record {
+                println!("{:?}", record);
+                self.grids.insert(record.name.clone(), record);
+            }
+        }
+    }
+}
+
+#[derive(serde::Deserialize, Debug, PartialEq, Eq, Hash)]
+pub struct Tileset {
+    pub name: String,
+    pub font: String,
+    pub weight: i32,
+    pub width: i32,
+    pub height: i32,
+}
+
+#[derive(Resource, Default, Debug)]
+pub struct Tilesets {
+    pub tilesets: HashMap<String, Tileset>,
+}
+
+impl Tilesets {
+    pub fn add(&mut self, path: &str, fonts: &mut Fonts) {
+        let Ok(mut csv) = csv::ReaderBuilder::new()
+            .delimiter(b'|')
+            .comment(Some(b'#'))
+            .trim(Trim::All)
+            .from_path(format!("assets/{}", path).as_str()) else { return; };
+
+        for record in csv.deserialize::<Tileset>() {
+            if let Ok(record) = record {
+                fonts.add(&record.name, &record.font);
+                self.tilesets.insert(record.name.clone(), record);
+            }
+        }
     }
 }
 
@@ -57,22 +255,110 @@ pub fn start_static_loading(mut next: ResMut<NextState<GameStates>>) {
 
 #[derive(Default)]
 pub struct SvarogLoadingPlugin {
-    loader: Option<Arc<dyn Fn(&mut Fonts) + 'static + Sync + Send>>,
+    loader: Option<Box<dyn Fn(&mut Tilesets, &mut Fonts, &mut Grids) + 'static + Sync + Send>>,
 }
 
 impl SvarogLoadingPlugin {
-    pub fn with_loader<F: Fn(&mut Fonts) + 'static + Sync + Send>(mut self, f: F) -> Self {
-        self.loader = Some(Arc::new(f));
+    pub fn with_loader<F: Fn(&mut Tilesets, &mut Fonts, &mut Grids) + 'static + Sync + Send>(mut self, f: F) -> Self {
+        self.loader = Some(Box::new(f));
         self
     }
 }
+
+#[derive(Component)]
+pub struct GridTag(pub String);
+
+#[derive(Component)]
+pub struct CameraTag;
+
+pub fn create_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera2dBundle {
+            transform: Transform::from_translation(Vec3::new(0.0, 10.0, 0.0)),
+            ..Default::default()
+        }, 
+        Visibility::Visible,
+        InheritedVisibility::default(),
+        CameraTag));
+}
+
+pub fn create_grid_entities(
+    mut commands: Commands, 
+    mut grids: ResMut<Grids>,
+    assets: Res<GameAssets>, 
+    tilesets: Res<Tilesets>, 
+    window: Query<&Window, With<PrimaryWindow>>,
+    camera: Query<Entity, With<CameraTag>>,
+    mut next: ResMut<NextState<GameStates>>) {
+
+    let Ok(window) = window.get_single() else { println!("NO WINDOW!"); return; };
+    let Ok(camera) = camera.get_single() else { println!("NO CAMERA!"); return; };
+
+    for (_, grid) in &mut grids.grids {
+        if grid.kind == GridKind::Glyph {
+            let Some(tileset) = tilesets.tilesets.get(&grid.tileset) else { 
+                println!("NO TILESET: {}", grid.tileset);
+                return; 
+            };
+
+            let (pos, camera_aligned) = {
+                if let Some(pos) = grid.align(tileset, window) {
+                    (pos, true)
+                } else {
+                    (Vec3::ZERO, false)
+                }
+            };
+
+            let id = commands
+                .spawn((
+                    GridTag(grid.name.to_string()),
+                    Transform::from_translation(pos),
+                    GlobalTransform::default(),
+                    Visibility::Visible,
+                    InheritedVisibility::default(),
+                ))
+                .with_children(|f| {
+                    for j in 0..grid.height {
+                        for i in 0..grid.width {
+                            let handle = f.spawn((SpriteSheetBundle {
+                                sprite: TextureAtlasSprite { index: (i as usize + j as usize) % 90, ..Default::default() },
+                                texture_atlas: assets.get(&tileset.name).expect(&format!("NO FONT: {}", tileset.name)),
+                                transform: Transform::from_translation(Vec3::new(
+                                    (i * tileset.width) as f32, 
+                                    (j * tileset.height) as f32, 
+                                    grid.depth as f32)),
+                                visibility: Visibility::Visible,
+                                ..Default::default()
+                            },)).id();
+    
+                            grid.entities.push(handle);
+                        }
+                    }
+                }).id();
+
+            grid.entity = Some(id);
+
+            if camera_aligned {
+                commands.entity(camera).push_children(&[id]);
+            }
+        }
+    }
+
+    next.set(GameStates::Game);
+}
+
 impl Plugin for SvarogLoadingPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
+        let mut tilesets = Tilesets::default();
         let mut fonts = Fonts::default();
-        (self.loader.as_ref().expect("Expected loader function"))(&mut fonts);
+        let mut grids = Grids::default();
+        (self.loader.as_ref().expect("Expected loader function"))(&mut tilesets, &mut fonts, &mut grids);
+        app.insert_resource(tilesets);
         app.insert_resource(fonts);
+        app.insert_resource(grids);
 
         app.add_systems(OnEnter(GameStates::StaticLoading), start_static_loading);
+        app.add_systems(OnEnter(GameStates::StaticLoading), create_camera);
  
         app.add_state::<GameStates>().add_loading_state(
             LoadingState::new(GameStates::AssetLoading)
@@ -80,5 +366,7 @@ impl Plugin for SvarogLoadingPlugin {
                 .with_dynamic_assets_file::<StandardDynamicAssetCollection>("resources.assets.ron")
                 .continue_to_state(GameStates::Setup),
         );
+
+        app.add_systems(OnEnter(GameStates::Setup), create_grid_entities);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use bevy::{app::App, asset::{Assets, Handle}, ecs::{schedule::{NextState, OnEnter, States}, system::{Commands, Res, ResMut, Resource}}, sprite::TextureAtlas};
+use bevy::{app::{App, Update}, asset::Handle, core_pipeline::core_2d::{Camera2d, Camera2dBundle}, ecs::{query::With, schedule::{common_conditions::in_state, IntoSystemConfigs, OnEnter, States}, system::{Commands, Query, Res, ResMut, Resource}}, hierarchy::BuildChildren, input::{keyboard::KeyCode, Input}, math::{Quat, Vec3}, render::view::{InheritedVisibility, Visibility}, sprite::{SpriteSheetBundle, TextureAtlas, TextureAtlasSprite}, transform::components::{GlobalTransform, Transform}};
 use bevy_asset_loader::asset_collection::AssetCollection;
-use loading::{Fonts, SvarogLoadingPlugin};
+use loading::{CameraTag, Fonts, Grids, SvarogLoadingPlugin, Tilesets};
 use windows::SvarogWindowPlugin;
+
+use crate::loading::GridKind;
 
 pub mod windows;
 pub mod loading;
@@ -18,24 +20,54 @@ pub enum GameStates {
 
 #[derive(AssetCollection, Resource)]
 pub struct GameAssets {
-    #[asset(key = "kenney")]
-    pub kenney: Handle<TextureAtlas>,
+    #[asset(key = "kenney_colour")]
+    pub kenney_colour: Handle<TextureAtlas>,
+    #[asset(key = "kenney_mono")]
+    pub kenney_mono: Handle<TextureAtlas>,
     #[asset(key = "source_code")]
     pub source_code: Handle<TextureAtlas>,
 }
 
-pub fn load_fonts(fonts: &mut Fonts) {
-    fonts.add("kenney", "kenney/kenney.font.csv");
-    fonts.add("sourcecodepro", "sourcecodepro/sourcecodepro.font.csv");
+impl GameAssets {
+    pub fn get(&self, name: &str) -> Option<Handle<TextureAtlas>> {
+        match name {
+            "kenney-colour" => Some(self.kenney_colour.clone_weak()),
+            "kenney-mono" => Some(self.kenney_mono.clone_weak()),
+            "sourcecodepro" => Some(self.source_code.clone_weak()),
+            _ => None
+        }
+    }
+}
+
+pub fn load_fonts(tilesets: &mut Tilesets, fonts: &mut Fonts, grids: &mut Grids) {
+    tilesets.add("tilesets.csv", fonts);
+    grids.add("grids.csv");
+}
+
+pub fn move_camera(mut camera: Query<&mut Transform, With<CameraTag>>, input: Res<Input<KeyCode>>) {
+    let mut dir = Vec3::ZERO;
+    if input.pressed(KeyCode::Left) {
+        dir.x = -5.0;
+    } else if input.pressed(KeyCode::Right) {
+        dir.x = 5.0;
+    }
+
+    if input.pressed(KeyCode::Up) {
+        dir.y = -5.0;
+    } else if input.pressed(KeyCode::Down) {
+        dir.y = 5.0;
+    }
+
+    if let Ok(mut camera) = camera.get_single_mut() {
+        camera.translation += dir;
+    }
 }
 
 pub fn main() {
     let mut app = App::default();
     app.add_plugins(SvarogWindowPlugin);
-    app.add_plugins(SvarogLoadingPlugin::default().with_loader(load_fonts)); 
-    app.add_systems(OnEnter(GameStates::Setup), 
-    |fonts: Res<Fonts>| { 
-        println!("{:?}", fonts);
-    });
+    app.add_plugins(SvarogLoadingPlugin::default()
+        .with_loader(load_fonts));
+    app.add_systems(Update, move_camera.run_if(in_state(GameStates::Game)));
     app.run();
 }
